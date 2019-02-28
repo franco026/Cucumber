34b257ad471948bf58b662827884e0daf68e189c
ORM <franco.kevin@correounivalle.edu.co>
2019-02-28 23:50:18 +0000

    ORM

--- routes/contact.js
+++ routes/contact.js
@@ -2,12 +2,51 @@
 var express = require('express');
 var Contact = require('../models/contact');
 var router = express.Router();
+
 router.route('/')
-.get(function(req, res) {
+.get((req, res) => {
     Contact
-    .fetchAll()
-    .then(function(contacts) {
+      .where(req.query)
+      .fetchAll()
+      .then((contacts) => {
         res.json({ contacts });
-    });
-});
+      });
+  })
+  .post((req, res) => {
+    new Contact({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      emailAddress: req.body.emailAddress,
+    })
+      .save()
+      .then((saved) => {
+        res.json({ saved });
+      });
+  });
+  
+router.route('/:id')
+  .put((req, res) => {
+    Contact
+      .where('id', req.params.id)
+      .fetch()
+      .then((contact) => {
+        contact.save({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          emailAddress: req.body.emailAddress,
+        })
+        .then((saved) => {
+          res.json({ saved });
+        });
+      });
+  })
+  .delete((req, res) => {
+    Contact
+      .where('id', req.params.id)
+      .destroy()
+      .then((destroyed) => {
+        res.json({ destroyed });
+      });
+  });
+
 module.exports = router;
