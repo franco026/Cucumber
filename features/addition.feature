dce8d687fd8c54baa5969fdcc8f3c34f0b4b8ff9
ORM <franco.kevin@correounivalle.edu.co>
2019-03-13 22:21:12 +0000

    fdsfs

---
+++ features/addition.feature
@@ -0,0 +1,5 @@
+Feature: Addition
+    Scenario: Colombia
+        Given yo entro a la pagina
+        When I make an API call using promise
+        Then I mostrarme "Americas"
---
+++ features/steps/maths.js
@@ -0,0 +1,38 @@
+/*eslint-disable no-unused-vars, unknown-require*/
+const { Given, When, Then, setDefaultTimeout } = require('cucumber');
+var request = require('request');
+const assert = require('assert');
+const fetch = require("node-fetch");
+
+let ruta;
+let resultado = {};
+let palabra;
+
+Given('yo entro a la pagina',function() {
+    ruta = 'https://restcountries.eu';
+});
+
+When('I make an API call using promise',function() {
+    fetch(ruta+'/rest/v2/name/colombia')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            resultado = myJson[0]
+            palabra = resultado['region'].toString()
+        });
+});
+
+Then('I mostrarme {string}',function (input){
+   fetch(ruta+'/rest/v2/name/colombia')
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(myJson) {
+            resultado = myJson[0]
+            palabra = resultado['region'].toString()
+            console.log(palabra)
+            assert.equal(palabra, input); 
+        });
+});
+
